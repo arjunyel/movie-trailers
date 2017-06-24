b7bd8143b5b26f9332d1543e5633b3b87496348c
arjunyel <arjunyel@gmail.com>
2017-06-24 12:55:12 -0500

    Title set correctly

--- movie-card.js
+++ movie-card.js
@@ -1,10 +1,16 @@
 "use strict";
 class MovieCard extends HTMLElement {
+    constructor() {
+        super();
+        const t = document.currentScript.ownerDocument.getElementById("movie-card");
+        const shadowRoot = this.attachShadow({ mode: "open" });
+        shadowRoot.appendChild(t.content.cloneNode(true));
+    }
     get oberservedAttributes() {
         return ["title", "poster_image_url", "trailer_youtube_url"];
     }
     get title() {
-        const title = this.getAttribute("value");
+        const title = this.getAttribute("title");
         if (title) {
             return title;
         }
@@ -13,26 +19,29 @@ class MovieCard extends HTMLElement {
     set title(t) {
         if (t) {
             this.setAttribute("title", t);
-            const title = this.getNode("title");
+            const title = this.getNode("#movie");
             if (title) {
-                title.innerHTML = this.getAttribute("t");
+                title.textContent = this.getAttribute("title");
             }
         }
         else {
-            this.removeAttribute("value");
+            this.removeAttribute("title");
         }
-    }
-    constructor() {
-        super();
-        const t = document.currentScript.ownerDocument.getElementById("movie-card");
-        const shadowRoot = this.attachShadow({ mode: "open" });
-        shadowRoot.appendChild(t.content.cloneNode(true));
     }
     getNode(id) {
         const shadow = this.shadowRoot;
-        return shadow.getElementById(id);
+        return shadow.querySelector(id);
     }
-    attributeChangedCallback(attrName, oldVal, newVal) {
+    attributeChangedCallback(attr, oldVal, newVal) {
+        if (attr === "title") {
+            this.title = newVal;
+        }
+        else {
+            console.log(newVal);
+        }
+    }
+    connectedCallback() {
+        this.title = this.getAttribute("title");
     }
 }
 window.customElements.define("movie-card", MovieCard);
--- movie-card.ts
+++ movie-card.ts
@@ -2,12 +2,19 @@
 class MovieCard extends HTMLElement {
     // title, poster_image_url, trailer_youtube_url
 
+    constructor() {
+        super(); // always call super() first in the ctor.
+        const t = document.currentScript.ownerDocument.getElementById("movie-card") as HTMLTemplateElement;
+        const shadowRoot = this.attachShadow({mode: "open"});
+        shadowRoot.appendChild(t.content.cloneNode(true));
+    }
+
     get oberservedAttributes() {
          return ["title", "poster_image_url", "trailer_youtube_url"];
      }
 
     public get title(): string {
-        const title = this.getAttribute("value");
+        const title = this.getAttribute("title");
         if (title) {
             return title;
         }
@@ -17,30 +24,33 @@ class MovieCard extends HTMLElement {
     public set title(t: string) {
          if (t) {
              this.setAttribute("title", t);
-             const title = this.getNode("title");
+             const title = this.getNode("#movie");
              if (title) {
                  // The next line uses getAttribute instead of the t variable to allow concurrent access to the DOM
-                 title.innerHTML = this.getAttribute("t") as string;
+                 title.textContent = this.getAttribute("title") as string;
              }
          } else {
-             this.removeAttribute("value");
+             this.removeAttribute("title");
          }
      }
 
-    constructor() {
-        super(); // always call super() first in the ctor.
-        const t = document.currentScript.ownerDocument.getElementById("movie-card") as HTMLTemplateElement;
-        const shadowRoot = this.attachShadow({mode: "open"});
-        shadowRoot.appendChild(t.content.cloneNode(true));
-    }
+
 
     private getNode(id: string) {
         const shadow = this.shadowRoot as ShadowRoot;
-        return shadow.getElementById(id);
+        return shadow.querySelector(id);
     }
 
-    private attributeChangedCallback(attrName: string, oldVal: string, newVal: string) {
-        
+    private attributeChangedCallback(attr: string, oldVal: string, newVal: string) {
+        if (attr === "title") {
+            this.title = newVal;
+        } else {
+            console.log(newVal);
+        }
+    }
+
+    private connectedCallback() {
+        this.title = this.getAttribute("title") as string;
     }
 }
 
