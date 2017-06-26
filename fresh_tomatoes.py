41a46f745c2815bbee7da22c1db4b6df4f350a37
arjunyel <arjunyel@gmail.com>
2017-06-26 15:01:13 -0500

    Stamping from api

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -121,9 +121,9 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{youtube}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster}" width="220" height="342">
+    <h2>{title}</h2>
 </div>
 '''
 
@@ -132,19 +132,11 @@ def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
-        # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
-
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
-            movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            title=movie.title,
+            poster=movie.poster,
+            youtube=movie.youtube,
         )
     return content
 
